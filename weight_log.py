6f940695f00c70647302af1689f76e7ee83c62df
shibatamasayuki <masayuki.shibata.0831@gmail.com>
2019-11-12 13:52:20 +0900

    体重の更新機能の追加

--- weight_log.py
+++ weight_log.py
@@ -3,7 +3,7 @@ import sqlite3
 
 def main():
     while True:
-        select_menu = input('メニューは?(登録: r ､閲覧: s ､削除: d ､終了: q ) > ')
+        select_menu = input('メニューは?\n(登録: r ､閲覧: s ､削除: d ､更新: u ､終了: q ) > ')
         # select_menu = 'd'
 
         if select_menu == 'r':
@@ -26,6 +26,17 @@ def main():
             delete_name = input('削除したい名前は? > ')
 
             delete_user(delete_name)
+
+        elif select_menu == 'u':
+            users = find_all_users()
+
+            for user in users:
+                print(f'name: {user[0]}, weight: {user[1]}')
+
+            update_name = input('更新したい名前は? > ')
+            update_weight = input('更新後の体重は? > ')
+
+            update_user(update_name, update_weight)
 
         elif select_menu == 'q':
             break
@@ -53,11 +64,21 @@ def find_all_users():
     return users
 
 
+
 def delete_user(delete_name):
     connection = sqlite3.connect('camp.db')
     cursor = connection.cursor()
     sql = f"DELETE FROM users  WHERE name = ?"
     cursor.execute(sql, (delete_name,))
+    connection.commit()
+    connection.close()
+
+
+def update_user(update_name, update_weight):
+    connection = sqlite3.connect('camp.db')
+    cursor = connection.cursor()
+    sql = f"UPDATE users SET weight ='{update_weight}' WHERE name = '{update_name}'"
+    cursor.execute(sql)
     connection.commit()
     connection.close()
 
